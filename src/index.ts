72cdf7330c15555e217dbe375043c69662f10e37
Marshall <mn.acunab@gmail.com>
2022-07-26 16:50:57 -0300

    common routes

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import DbConfig from './db/dbConfig';
+import passport from 'passport';
 
 DbConfig.connectDb(process.env.DB_CNN as string);
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app: Application = express();
 
+app.use(passport.initialize());
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
