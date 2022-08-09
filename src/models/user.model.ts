11f54d95727b3cd6eb8bab2b48a421f95bec0711
Marshall <mn.acunab@gmail.com>
2022-08-09 16:31:57 -0300

    reformat model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -21,42 +21,4 @@ const userSchema = new Schema({
 
 const userModel = model<IUser & Document>('User', userSchema);
 
-export default userModel; 
-/*
-class UserModel {
-    schema = dbConfig.getMongoose().Schema;
-    constructor() {};
-    userSchema = new this.schema({
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        googleId: String,
-        password: {
-            type: String
-        }
-    });
-    UserDb = dbConfig.getMongoose().model<UserDocument>('User', this.userSchema);
-    async getUser(id: string) {
-        const user = await this.UserDb.findById(id);
-        return user;
-    }
-    async findOneUser(googleId: string) {
-        const user = await this.UserDb.findOne({ googleId: googleId });
-        return user;
-    }
-    async findById(id: string) {
-        const user = await this.UserDb.findById(id);
-        return user;
-    }
-    async createUser(user: any) {
-        const newUser = await this.UserDb.create(user);
-        return newUser;
-    }
-}
-
-export default new UserModel();*/
+export default userModel;
